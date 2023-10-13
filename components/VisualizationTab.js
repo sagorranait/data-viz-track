f939f0142327a7dd2557155e24984e5b6df06c6b
sagorranait <sagorranait@gmail.com>
2023-10-13 15:56:51 +0600

    Functionality of VisualizationTab is done.

--- components/VisualizationTab.js
+++ components/VisualizationTab.js
@@ -1,73 +1,69 @@
-import React from 'react'
-import { BarChart, Bar, Cell, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
-
-
+"use client";
+import React, { useState } from 'react'
+import { 
+  Bar, 
+  XAxis, 
+  YAxis, 
+  Tooltip, 
+  BarChart, 
+  ResponsiveContainer, 
+  PieChart,
+  Pie,
+  LineChart,
+  Line
+} from 'recharts';
 
 const VisualizationTab = ({ previousTabIndex, tabHandler }) => {
-  const data = [
-    {
-      name: 'Page A',
-      uv: 4000,
-      pv: 2400,
-      amt: 2400,
-    },
-    {
-      name: 'Page B',
-      uv: 3000,
-      pv: 1398,
-      amt: 2210,
-    },
-    {
-      name: 'Page C',
-      uv: 2000,
-      pv: 9800,
-      amt: 2290,
-    },
-    {
-      name: 'Page D',
-      uv: 2780,
-      pv: 3908,
-      amt: 2000,
-    },
-    {
-      name: 'Page E',
-      uv: 1890,
-      pv: 4800,
-      amt: 2181,
-    },
-    {
-      name: 'Page F',
-      uv: 2390,
-      pv: 3800,
-      amt: 2500,
-    },
-    {
-      name: 'Page G',
-      uv: 3490,
-      pv: 4300,
-      amt: 2100,
-    },
-  ];
+  const [setselectVisualization, setsetselectVisualization] = useState('bar');
+  const { userType, convertList } = JSON.parse(localStorage.getItem('dataviztrack'));
+
+  const selectVisualizationHandler = (event) => {
+    console.log(typeof(event.target.value));
+    setsetselectVisualization(event.target.value);
+  }
 
   return (
     <div className="p-4">
       <div className='mb-8 md:mb-14'>
-        <ResponsiveContainer width="100%" height={350}>
-          <BarChart data={data} >
-            <XAxis dataKey="name" />
-            <YAxis />
-            <Tooltip />
-            <Bar dataKey="pv" fill="#8884d8" />
-            <Bar dataKey="uv" fill="#82ca9d" />
-          </BarChart>
-        </ResponsiveContainer>
+        { setselectVisualization === 'bar' && 
+          <ResponsiveContainer width="100%" height={350}>
+            <BarChart data={convertList[convertList.length - 1]} >
+              <XAxis dataKey="Employee Name" />
+              <YAxis dataKey="Employee ID" />
+              <Tooltip />
+              <Bar dataKey="Employee ID" fill="#8884d8" />
+            </BarChart>
+          </ResponsiveContainer>
+        }
+        { setselectVisualization === 'pie' && 
+          <ResponsiveContainer width="100%" height={350}>            
+            <PieChart>
+              <Pie dataKey="Employee ID" data={convertList[convertList.length - 1]} cx="50%" cy="50%" outerRadius={80} fill="#8884d8" label/>
+                <Pie dataKey="Employee ID" fill="#82ca9d" />
+              <Tooltip />
+            </PieChart>
+          </ResponsiveContainer>
+        }
+        { setselectVisualization === 'line' && 
+          <ResponsiveContainer width="100%" height={350}>
+            <LineChart data={convertList[convertList.length - 1]} >
+              <XAxis dataKey="Employee Name" />
+              <YAxis dataKey="Employee ID" />
+              <Tooltip />
+              <Line type="monotone" dataKey="Employee ID" stroke="#8884d8" />
+            </LineChart>
+          </ResponsiveContainer>
+        }
+        {setselectVisualization !== 'bar' && setselectVisualization !== 'pie' && setselectVisualization !== 'line' && (
+          <p>Invalid visualization selected.</p>
+        )}
       </div>
       <div className='flex flex-wrap items-center justify-center flex-col gap-5 md:gap-0 md:justify-between md:flex-row'>
         <div className='flex flex-wrap items-center justify-center gap-2 flex-row'>
           <p>Select Visualization : </p>
           <div className="w-48 relative">
-            <select className="block appearance-none w-full bg-white border border-gray-300 text-gray-700 py-2 px-3 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500 focus:text-gray-900">
-              <option value="bar" selected>Bar Chart</option>
+            <select className="block appearance-none w-full bg-white border border-gray-300 text-gray-700 py-2 px-3 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500 focus:text-gray-900" onChange={selectVisualizationHandler}>
+              <option value="bar" defaultValue>Bar Chart</option>
               <option value="pie">Pie Chart</option>
               <option value="line">Line Graph</option>
             </select>
